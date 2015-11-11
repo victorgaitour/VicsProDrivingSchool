0cbaf2de6b3fd933ea268ee87f10d4b8f58bfe1b
victorgaitour <victorgaitour@yahoo.com>
2015-11-11 16:22:03 -0500

    base

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory, session
+app = Flask('__name__')
+app.config['SECRET_KEY'] = "Alex is great"
+
+@app.route("/", methods = ["POST", "GET"])
+@app.route("/home", methods = ["POST", "GET"])
+def home():
+    return render_template("home.html")
+
+
+
+
+if __name__=="__main__":
+    app.debug = True
+    app.run(port=8000)
+
